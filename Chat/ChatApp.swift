85b888e6b2d4be8ee009b66317fdb8aa2b0fef1b
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-26 15:07:00 +0000

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Shaquille McGregor on 26/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
