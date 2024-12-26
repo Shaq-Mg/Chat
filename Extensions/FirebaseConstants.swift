ef725d2f245d547ef5e2eb0b7bcd5effb31b64a3
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-26 15:15:56 +0000

    User & Message data models with firebase constants

---
+++ Extensions/FirebaseConstants.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseConstants.swift
+//  Chat
+//
+//  Created by Shaquille McGregor on 26/12/2024.
+//
+
+import Foundation
---
+++ Models/.swift
@@ -0,0 +1,8 @@
+//
+//  Message.swift
+//  Chat
+//
+//  Created by Shaquille McGregor on 26/12/2024.
+//
+
+import Foundation
