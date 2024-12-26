8c4f761f5f95399f4a80199bebf5898e76b8f2b5
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-26 20:04:55 +0000

    SettingsView UI ⚙️

---
+++ Views/Login/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  Chat
+//
+//  Created by Shaquille McGregor on 26/12/2024.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
